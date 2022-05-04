661b21dad030cb3f3127db1f138f82f80504032b
luciabrumnich <luciabrumnichhr@gmail.com>
2022-05-04 09:14:02 -0300

    modification async fn to access the word bank and select the todays word

--- src/Words.js
+++ src/Words.js
@@ -9,16 +9,18 @@ export const boardDefault = [
   ["", "", "", "", ""],
 ];
 
+const getFile = async (fileURL) => {
+  let fileContent = await fetch(fileURL);
+  fileContent = await fileContent.text();
+  return fileContent;
+};
+
 export const generateWordSet = async () => {
   let wordSet;
   let todaysWord;
-  await fetch(wordBank)
-    .then((response) => response.text())
-    .then((result) => {
-      const wordArray = result.split("\n");
-      todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-      wordSet = new Set(wordArray);
-    });
-
+  const wordRaw = await getFile(wordBank);
+  const wordArray = wordRaw.split("\n");
+  todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+  wordSet = new Set(wordArray);
   return { wordSet, todaysWord };
 };
